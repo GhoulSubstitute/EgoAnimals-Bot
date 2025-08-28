60dcbb427a3b48992b84a3f7402ddf8f2a513384
Ghoul Marco <47378744+GhoulSubstitute@users.noreply.github.com>
2025-08-28 09:26:42 +0530

    Update main.py prefix

--- Animal Bot/main.py
+++ Animal Bot/main.py
@@ -9,7 +9,7 @@ class AnimalBot(commands.Bot):
         intents = discord.Intents.default()
         intents.messages = True
         intents.guilds = True
-        super().__init__(command_prefix="c!", intents=intents)
+        super().__init__(command_prefix="!", intents=intents)
 
     async def setup_hook(self):
         await self.load_extension("commands.animals")
@@ -18,4 +18,5 @@ class AnimalBot(commands.Bot):
 
 bot = AnimalBot()
 bot.run(TOKEN)
+
 
